b5a4623436e40b1089edaa290a4992622e4b6263
Matias <selinmatias5@gmail.com>
2023-06-08 23:43:24 +0300

    add .eslintrc.js

---
+++ backend/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: { es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+};
+
