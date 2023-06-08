cf05947e81581b6096e59409dadc94b425631b03
Matias <selinmatias5@gmail.com>
2023-06-08 23:34:32 +0300

    init server.ts express functionality

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,12 @@
-console.log('hi');
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${3000}`);
+});
