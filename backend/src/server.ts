28c5ae17b9575f2afab375d193652bd87270f87c
Matias <selinmatias5@gmail.com>
2023-06-08 23:52:18 +0300

    add workout endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,21 @@
 import express from 'express';
+import { Workout } from './types';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+let workouts: Workout[] = [
+  { date: '2023-3-21', notes: 'felt like crap' },
+  { date: '2023-3-29', notes: 'improving' },
+  { date: '2023-4-1', notes: 'hurt my shoulder' },
+];
+
+app.get('/', (_req, res) => {
   res.send('hi');
+});
+
+app.get('/api/workouts', (_req, res) => {
+  res.send(workouts);
 });
 
 app.listen(PORT, () => {
