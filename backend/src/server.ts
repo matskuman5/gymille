2e9e703b3d2c681e6b0537e1e22e10124b39d54e
Matias <selinmatias5@gmail.com>
2023-06-09 00:11:45 +0300

    add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { Workout } from './types';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3000;
 
