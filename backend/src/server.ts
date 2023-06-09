654a1c22724da9bab056c17edf6d878798adfd48
Matias <selinmatias5@gmail.com>
2023-06-09 18:01:32 +0300

    separate dummy sessions into own file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import { Session } from './types';
 import logger from './utils/logging';
 
+import dummySessions from './test/dummy-sessions.json';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -15,18 +17,18 @@ app.use((req, _res, next) => {
 
 const PORT = 3000;
 
-let workouts: Session[] = [];
+let sessions: Session[] = dummySessions;
 
 app.get('/', (_req, res) => {
   res.send('hi');
 });
 
 app.get('/api/sessions', (_req, res) => {
-  res.send(workouts);
+  res.send(sessions);
 });
 
 app.post('/api/sessions', (req, res) => {
-  workouts.push(req.body);
+  sessions.push(req.body);
   res.send('workout added successfully');
 });
 
