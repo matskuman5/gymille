03aa55b8b41ca9c0cb928300121e837990720eca
Matias <selinmatias5@gmail.com>
2023-06-09 22:03:21 +0300

    make postSession async

--- frontend/src/services/sessions.ts
+++ frontend/src/services/sessions.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import { Session } from '../types';
 
-export const postSession = (session: Session) => {
-  axios.post('http://localhost:3000/api/sessions', session);
+export const postSession = async (session: Session) => {
+  const response = await axios.post(
+    'http://localhost:3000/api/sessions',
+    session
+  );
+  return response.data;
 };
 
 export const getSessions = async () => {
