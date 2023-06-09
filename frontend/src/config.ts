0058fb0cd95fa1c2b8a631c831691b9dc51d3db4
Matias <selinmatias5@gmail.com>
2023-06-09 22:26:44 +0300

    extract api url to config file

---
+++ frontend/src/config.ts
@@ -0,0 +1,3 @@
+const apiUrl = import.meta.env.API_URL || 'http://localhost:3000';
+
+export default apiUrl;
--- frontend/src/services/sessions.ts
+++ frontend/src/services/sessions.ts
@@ -1,17 +1,13 @@
 import axios from 'axios';
 import { Session } from '../types';
+import apiUrl from '../config';
 
 export const postSession = async (session: Session) => {
-  const response = await axios.post(
-    'http://localhost:3000/api/sessions',
-    session
-  );
+  const response = await axios.post(`${apiUrl}/api/sessions`, session);
   return response.data;
 };
 
 export const getSessions = async () => {
-  const response = await axios.get<Session[]>(
-    'http://localhost:3000/api/sessions'
-  );
+  const response = await axios.get<Session[]>(`${apiUrl}/api/sessions`);
   return response.data;
 };
