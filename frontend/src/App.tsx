1cbcab97dfc8ce57880350ad2af63002375944f9
Matias <selinmatias5@gmail.com>
2023-06-09 22:49:00 +0300

    add Header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,12 @@
 import { Link, Route, Routes } from 'react-router-dom';
 import NewSessionForm from './components/new-session/NewSessionForm';
 import SessionList from './components/session-list/SessionList';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">New Session</Link>
-          </li>
-          <li>
-            <Link to="/sessions">Sessions</Link>
-          </li>
-        </ul>
-      </nav>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<NewSessionForm />}></Route>
         <Route path="/sessions" element={<SessionList />}></Route>
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { AppBar, Button, Toolbar } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <Button variant="contained" component={RouterLink} to="/">
+          New Session
+        </Button>
+        <Button variant="contained" component={RouterLink} to="/sessions">
+          Previous Sessions
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
