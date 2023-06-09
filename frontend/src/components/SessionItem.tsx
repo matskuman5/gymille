6bfe8ff2589277e9a0a22d84a642c95f470097cb
Matias <selinmatias5@gmail.com>
2023-06-09 19:02:33 +0300

    separate individual sessions into components

---
+++ frontend/src/components/SessionItem.tsx
@@ -0,0 +1,50 @@
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from '@mui/material';
+import { Session } from '../types';
+
+interface Props {
+  session: Session;
+}
+
+const SessionItem = ({ session }: Props) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              <b>{session.date}</b>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Exercise</TableCell>
+            <TableCell>Sets</TableCell>
+            <TableCell>Reps</TableCell>
+            <TableCell>Weight</TableCell>
+            <TableCell>Notes</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {session.exercises.map((exercise) => (
+            <TableRow key={exercise.name}>
+              <TableCell>{exercise.name}</TableCell>
+              <TableCell>{exercise.sets}</TableCell>
+              <TableCell>{exercise.reps}</TableCell>
+              <TableCell>{exercise.weight}</TableCell>
+              <TableCell>{exercise.notes}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default SessionItem;
--- frontend/src/components/SessionList.tsx
+++ frontend/src/components/SessionList.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Session } from '../types';
+import SessionItem from './SessionItem';
 
 const SessionList = () => {
   const [sessions, setSessions] = useState<Session[]>([]);
@@ -14,29 +15,9 @@ const SessionList = () => {
   return (
     <div>
       <h2>Past workout sessions:</h2>
-      <ul>
-        {sessions.map((session) => {
-          return (
-            <li key={session.date}>
-              <div>
-                <h3>Date: {session.date}</h3>
-                {session.exercises.map((exercise) => {
-                  return (
-                    <div key={exercise.name}>
-                      <p>
-                        <b>{exercise.name}</b> ({exercise.bodyPart}),{' '}
-                        {exercise.sets} sets of {exercise.reps} at{' '}
-                        {exercise.weight}
-                      </p>
-                      <p>Notes: {exercise.notes}</p>
-                    </div>
-                  );
-                })}
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      {sessions.map((session) => (
+        <SessionItem key={session.date} session={session}></SessionItem>
+      ))}
     </div>
   );
 };
