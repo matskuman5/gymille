bcab56d94e614820487420de6621c520049ff809
Matias <selinmatias5@gmail.com>
2023-06-11 17:31:15 +0300

    allow adding and deleting session templates

--- frontend/src/components/session-templates/SessionTemplateList.tsx
+++ frontend/src/components/session-templates/SessionTemplateList.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { SessionTemplate } from '../../types';
 import SessionTemplateItem from './SessionTemplateItem';
 import { getSessionTemplates } from '../../services/session-templates';
+import { Button } from '@mui/material';
 
 const SessionTemplateList = () => {
   const [sessionTemplates, setSessionTemplates] = useState<SessionTemplate[]>(
@@ -17,16 +18,39 @@ const SessionTemplateList = () => {
     fetchSessionTemplates();
   }, []);
 
+  const newSessionTemplate = () => {
+    setSessionTemplates([
+      ...sessionTemplates,
+      {
+        name: '',
+        exerciseTypes: [],
+      },
+    ]);
+  };
+
+  const deleteSessionTemplate = (nameToRemove: string) => {
+    setSessionTemplates(
+      sessionTemplates.filter(
+        (sessionTemplate) => sessionTemplate.name !== nameToRemove
+      )
+    );
+  };
+
   return (
     <div>
       <h2>Session templates:</h2>
       {sessionTemplates.map((sessionTemplate) => (
         // TODO: replace key with uuid
-        <SessionTemplateItem
-          key={sessionTemplate.name}
-          givenSessionTemplate={sessionTemplate}
-        ></SessionTemplateItem>
+        <div key={sessionTemplate.name}>
+          <SessionTemplateItem
+            givenSessionTemplate={sessionTemplate}
+          ></SessionTemplateItem>
+          <Button onClick={() => deleteSessionTemplate(sessionTemplate.name)}>
+            Delete
+          </Button>
+        </div>
       ))}
+      <Button onClick={newSessionTemplate}>Add New Session Template</Button>
     </div>
   );
 };
