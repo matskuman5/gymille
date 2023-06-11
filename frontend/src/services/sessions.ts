d2cdc4795987713aef6f9854e114edd359ebe8db
Matias <selinmatias5@gmail.com>
2023-06-12 01:07:25 +0300

    add notification when adding sessions

--- frontend/src/services/sessions.ts
+++ frontend/src/services/sessions.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import { Session } from '../types';
 import apiUrl from '../config';
-import { showError } from '../utils/notifications';
+import { showError, showNotification } from '../utils/notifications';
 
 export const postSession = async (session: Session) => {
   try {
     const response = await axios.post(`${apiUrl}/api/sessions`, session);
+    showNotification('Session saved successfully.', 'success');
     return response.data;
   } catch (error) {
     showError(error);
