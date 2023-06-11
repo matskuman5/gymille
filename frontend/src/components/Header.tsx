1028e453aafdb8d030b7f3530c152926553d82fb
Matias <selinmatias5@gmail.com>
2023-06-12 01:05:36 +0300

    add cool title to header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,17 @@
-import { AppBar, Button, Toolbar } from '@mui/material';
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <AppBar position="sticky">
+      <Typography
+        variant="h3"
+        fontWeight="bold"
+        fontStyle="italic"
+        style={{ margin: 16 }}
+      >
+        Gymille!
+      </Typography>
       <Toolbar>
         <Button variant="contained" component={RouterLink} to="/">
           New Session
