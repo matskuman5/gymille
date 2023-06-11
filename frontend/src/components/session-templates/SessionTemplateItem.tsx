61725d102e2b033883e0d84ac4937d113b4ac174
Matias <selinmatias5@gmail.com>
2023-06-11 23:15:10 +0300

    remove mysterious empty space

--- frontend/src/components/session-templates/SessionTemplateItem.tsx
+++ frontend/src/components/session-templates/SessionTemplateItem.tsx
@@ -24,7 +24,7 @@ const SessionTemplateItem = ({ givenSessionTemplate }: Props) => {
       ) : (
         <>
           <h3>
-            {sessionTemplate.name}{' '}
+            {sessionTemplate.name}
             <IconButton onClick={() => setEditing(!editing)}>
               <EditIcon />
             </IconButton>
