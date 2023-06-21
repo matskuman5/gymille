ed01011e37b96215b2ef60c2f6b68c7531cee8ae
Matias <selinmatias5@gmail.com>
2023-06-21 21:45:02 +0300

    connect to db when starting backend

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,3 +1,5 @@
-const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 3000;
 
-export default PORT;
+export const DATABASE_URL =
+  process.env.DATABASE_URL ||
+  'postgres://postgres:secret@localhost:5432/postgres';
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,8 +3,9 @@ import cors from 'cors';
 import logger from './utils/logging';
 import pingRouter from './routers/ping';
 import sessionRouter from './routers/session';
-import PORT from './config';
+import { PORT, DATABASE_URL } from './config';
 import sessionTemplateRouter from './routers/session-templates';
+import { Sequelize } from 'sequelize';
 
 const app = express();
 app.use(express.json());
@@ -19,6 +20,20 @@ app.use((req, _res, next) => {
 app.use('/ping', pingRouter);
 app.use('/api/sessions', sessionRouter);
 app.use('/api/session-templates', sessionTemplateRouter);
+
+const sequelize = new Sequelize(DATABASE_URL);
+
+const connectToDB = async () => {
+  try {
+    await sequelize.authenticate();
+    logger.info('Connected to database successfully');
+    sequelize.close();
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+connectToDB();
 
 app.listen(PORT, () => {
   logger.info(`Server listening on port ${PORT}`);
