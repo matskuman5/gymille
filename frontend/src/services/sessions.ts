9c59d2a685b36bc7b84f9d39bc468b7d8cf3fc17
Matias <selinmatias5@gmail.com>
2023-06-23 23:52:23 +0300

    remove unnecessary response validation

--- frontend/src/services/sessions.ts
+++ frontend/src/services/sessions.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { Session } from '../types';
 import apiUrl from '../config';
 import { showError, showNotification } from '../utils/notifications';
-import { v4 as uuidv4 } from 'uuid';
 
 export const postSession = async (session: Session) => {
   try {
@@ -17,20 +16,7 @@ export const postSession = async (session: Session) => {
 export const getSessions = async () => {
   try {
     const response = await axios.get<Session[]>(`${apiUrl}/sessions`);
-
-    if (response.data !== undefined) {
-      const sessionDataWithIDs = response.data.map((session) => {
-        const exercisesWithIDs = session.exercises.map((exercise) => ({
-          ...exercise,
-          id: uuidv4(),
-        }));
-        return {
-          ...session,
-          exercises: exercisesWithIDs,
-        };
-      });
-      return sessionDataWithIDs;
-    }
+    return response.data;
   } catch (error) {
     showError(error);
   }
