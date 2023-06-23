736efd1586c6b08f998dd8dff0ad18584dcfe008
Matias <selinmatias5@gmail.com>
2023-06-23 19:48:32 +0300

    adjust default db password

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,4 +2,4 @@ export const PORT = process.env.PORT || 3000;
 
 export const DATABASE_URL =
   process.env.DATABASE_URL ||
-  'postgres://postgres:secret@localhost:5432/postgres';
+  'postgres://postgres:test@localhost:5432/postgres';
