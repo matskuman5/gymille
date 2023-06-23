03974d5c73adb51a92fdbc5551e2bcceb37199ce
Matias <selinmatias5@gmail.com>
2023-06-23 22:39:27 +0300

    expand type checking

--- backend/src/utils/types.ts
+++ backend/src/utils/types.ts
@@ -4,39 +4,23 @@ export interface SessionTemplate {
   exerciseTemplates: ExerciseTemplate[];
 }
 
+export const isSessionTemplate = (object: unknown): object is Session =>
+  typeof object === 'object' &&
+  object !== null &&
+  'id' in object &&
+  'name' in object &&
+  'exerciseTemplates' in object &&
+  typeof object.id === 'string' &&
+  typeof object.name === 'string' &&
+  Array.isArray(object.exerciseTemplates) &&
+  object.exerciseTemplates.every(isExerciseTemplate);
+
 export interface Session {
   id: string;
   date: string;
   name?: string;
   exercises: Exercise[];
 }
-
-export interface ExerciseTemplate {
-  name: string;
-  sets?: number;
-  reps?: number;
-}
-
-export interface Exercise extends ExerciseTemplate {
-  sets: number;
-  reps: number;
-  weight: number;
-  notes?: string;
-}
-
-export const isExercise = (object: unknown): object is Exercise =>
-  typeof object === 'object' &&
-  object !== null &&
-  'id' in object &&
-  'name' in object &&
-  'sets' in object &&
-  'reps' in object &&
-  'weight' in object &&
-  typeof object.id === 'string' &&
-  typeof object.name === 'string' &&
-  typeof object.sets === 'number' &&
-  typeof object.reps === 'number' &&
-  typeof object.weight === 'number';
 
 export const isSession = (object: unknown): object is Session =>
   typeof object === 'object' &&
@@ -48,3 +32,34 @@ export const isSession = (object: unknown): object is Session =>
   typeof object.date === 'string' &&
   Array.isArray(object.exercises) &&
   object.exercises.every(isExercise);
+
+export interface ExerciseTemplate {
+  id: string;
+  name: string;
+  sets?: number;
+  reps?: number;
+}
+
+export const isExerciseTemplate = (object: unknown): object is Exercise =>
+  typeof object === 'object' &&
+  object !== null &&
+  'id' in object &&
+  'name' in object &&
+  typeof object.id === 'string' &&
+  typeof object.name === 'string';
+
+export interface Exercise extends ExerciseTemplate {
+  sets: number;
+  reps: number;
+  weight: number;
+  notes?: string;
+}
+
+export const isExercise = (object: unknown): object is Exercise =>
+  isExerciseTemplate(object) &&
+  'sets' in object &&
+  'reps' in object &&
+  'weight' in object &&
+  typeof object.sets === 'number' &&
+  typeof object.reps === 'number' &&
+  typeof object.weight === 'number';
