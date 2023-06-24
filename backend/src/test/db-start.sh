c8352774771ed5bf667767ade4995c3d8aa7e1da
Matias <selinmatias5@gmail.com>
2023-06-24 20:51:21 +0300

    if db container already exists, start it

--- backend/src/test/db-start.sh
+++ backend/src/test/db-start.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-docker run -d --name testdb -e POSTGRES_PASSWORD=test -p 5432:5432 postgres
+
+if docker container ls -a --filter name=testdb --format '{{.Names}}' | grep -q '^testdb$'; then
+    docker start testdb
+else
+    docker run -d --name testdb -e POSTGRES_PASSWORD=test -p 5432:5432 postgres
 docker exec -it testdb psql -U postgres postgres
