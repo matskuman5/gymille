e5e50c1221e413b1d8158cd2bf5fc0fef72f4c3e
Matias <selinmatias5@gmail.com>
2023-06-30 17:05:10 +0300

    make footer stick to bottom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,24 @@ import SessionList from './components/session-list/SessionList';
 import Header from './components/Header';
 import SessionTemplateList from './components/session-templates/SessionTemplateList';
 import { ToastContainer } from 'react-toastify';
-import { Container, Stack } from '@mui/material';
+import { Box, Container, Stack } from '@mui/material';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Container>
-      <Stack spacing={2}>
+      <Stack spacing={2} minHeight="100vh">
         <Header />
-        <Routes>
-          <Route path="/" element={<NewSessionForm />}></Route>
-          <Route path="/sessions" element={<SessionList />}></Route>
-          <Route
-            path="/session-templates"
-            element={<SessionTemplateList />}
-          ></Route>
-        </Routes>
+        <Box sx={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<NewSessionForm />}></Route>
+            <Route path="/sessions" element={<SessionList />}></Route>
+            <Route
+              path="/session-templates"
+              element={<SessionTemplateList />}
+            ></Route>
+          </Routes>
+        </Box>
         <Footer />
       </Stack>
       <ToastContainer />
