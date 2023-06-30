93264a4d8ab653d3b020175c369c8fc47ac0cdff
Matias <selinmatias5@gmail.com>
2023-06-30 16:16:40 +0300

    add message if no sessions

--- frontend/src/components/session-list/SessionList.tsx
+++ frontend/src/components/session-list/SessionList.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Session } from '../../types';
 import SessionItem from './SessionItem';
 import { getSessions } from '../../services/sessions';
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { deleteSession as deleteSessionAPI } from '../../services/sessions';
 import Loading from '../Loading';
 
@@ -33,15 +33,21 @@ const SessionList = () => {
       {loading ? (
         <Loading text={'Fetching sessions...'} />
       ) : (
-        <Stack spacing={2}>
-          {sessions.map((session) => (
-            <SessionItem
-              key={session.id}
-              session={session}
-              deleteSession={deleteSession}
-            ></SessionItem>
-          ))}
-        </Stack>
+        <>
+          {sessions.length === 0 ? (
+            <Typography variant="h5">No sessions created yet!</Typography>
+          ) : (
+            <Stack spacing={2}>
+              {sessions.map((session) => (
+                <SessionItem
+                  key={session.id}
+                  session={session}
+                  deleteSession={deleteSession}
+                ></SessionItem>
+              ))}
+            </Stack>
+          )}
+        </>
       )}
     </>
   );
