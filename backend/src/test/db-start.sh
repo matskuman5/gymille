a90ef59f83bf17a3d49805acd587b9b9266f5d4d
Matias <selinmatias5@gmail.com>
2023-06-30 15:49:17 +0300

    adjust script formatting

--- backend/src/test/db-start.sh
+++ backend/src/test/db-start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-if docker container ls -a --filter name=testdb --format '{{.Names}}' | grep -q '^testdb$'; then
+if docker container ls -a --filter name=testdb --format '{{.Names}}' | grep -q '^testdb$';
+then
     docker start testdb
 else
     docker run -d --name testdb -e POSTGRES_PASSWORD=test -p 5432:5432 postgres
+fi
 docker exec -it testdb psql -U postgres postgres
