5782310ce279dedaed9dc83bfcc212d7138aca8a
Matias <selinmatias5@gmail.com>
2023-06-30 18:41:23 +0300

    add expand button to previous sessions

--- frontend/src/components/session-list/SessionItem.tsx
+++ frontend/src/components/session-list/SessionItem.tsx
@@ -7,9 +7,14 @@ import {
   TableCell,
   TableBody,
   Button,
+  IconButton,
+  Typography,
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Session } from '../../types';
+import { useState } from 'react';
 
 interface Props {
   session: Session;
@@ -17,19 +22,52 @@ interface Props {
 }
 
 const SessionItem = ({ session, deleteSession }: Props) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>
-              {session.name ? (
-                <b>
-                  {session.name}, {session.date}
-                </b>
-              ) : (
-                <b>{session.date}</b>
-              )}
+              <IconButton onClick={() => setExpanded(!expanded)}>
+                {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+              </IconButton>
+            </TableCell>
+            <TableCell>
+              <Typography variant="h5">
+                {session.name ? `${session.name}` : 'Untitled'}
+              </Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant="h5">{session.date}</Typography>
+            </TableCell>
+          </TableRow>
+          {expanded && (
+            <TableRow>
+              <TableCell>Exercise</TableCell>
+              <TableCell>Sets</TableCell>
+              <TableCell>Reps</TableCell>
+              <TableCell>Weight</TableCell>
+              <TableCell>Notes</TableCell>
+            </TableRow>
+          )}
+        </TableHead>
+        <TableBody>
+          {expanded &&
+            session.exercises.map((exercise) => (
+              <TableRow key={exercise.id}>
+                <TableCell>{exercise.name}</TableCell>
+                <TableCell>{exercise.sets}</TableCell>
+                <TableCell>{exercise.reps}</TableCell>
+                <TableCell>{exercise.weight}</TableCell>
+                <TableCell>{exercise.notes}</TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+        {expanded && (
+          <TableRow>
+            <TableCell>
               <Button
                 onClick={() => deleteSession(session.id)}
                 startIcon={<DeleteIcon />}
@@ -38,25 +76,7 @@ const SessionItem = ({ session, deleteSession }: Props) => {
               </Button>
             </TableCell>
           </TableRow>
-          <TableRow>
-            <TableCell>Exercise</TableCell>
-            <TableCell>Sets</TableCell>
-            <TableCell>Reps</TableCell>
-            <TableCell>Weight</TableCell>
-            <TableCell>Notes</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {session.exercises.map((exercise) => (
-            <TableRow key={exercise.id}>
-              <TableCell>{exercise.name}</TableCell>
-              <TableCell>{exercise.sets}</TableCell>
-              <TableCell>{exercise.reps}</TableCell>
-              <TableCell>{exercise.weight}</TableCell>
-              <TableCell>{exercise.notes}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+        )}
       </Table>
     </TableContainer>
   );
