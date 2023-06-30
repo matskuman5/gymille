6f196e022b0b91123bd4fee96b852aa4b5b0ded9
Matias <selinmatias5@gmail.com>
2023-06-30 18:04:05 +0300

    arrange exercise info fields in grid

--- frontend/src/components/new-session/ExerciseForm.tsx
+++ frontend/src/components/new-session/ExerciseForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack, TextField } from '@mui/material';
+import { Button, Grid, TextField } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useEffect, useState } from 'react';
 import { Exercise } from '../../types';
@@ -44,55 +44,69 @@ const ExerciseForm = ({
   };
 
   return (
-    <Stack direction="row" spacing={1}>
-      <TextField
-        label="Name"
-        name="name"
-        value={exerciseData.name}
-        onChange={handleInputChange}
-        error={!exerciseData.name}
-        helperText={!exerciseData.name ? 'Name must be non-empty' : ''}
-      ></TextField>
-      <TextField
-        label="Sets"
-        name="sets"
-        value={exerciseData.sets}
-        type="number"
-        onChange={handleInputChange}
-        error={exerciseData.sets <= 0}
-        helperText={exerciseData.sets <= 0 ? 'Sets must be at least 1' : ''}
-      ></TextField>
-      <TextField
-        label="Reps"
-        name="reps"
-        value={exerciseData.reps}
-        type="number"
-        onChange={handleInputChange}
-        error={exerciseData.reps <= 0}
-        helperText={exerciseData.reps <= 0 ? 'Reps must be at least 1' : ''}
-      ></TextField>
-      <TextField
-        label="Weight"
-        name="weight"
-        value={exerciseData.weight}
-        type="number"
-        onChange={handleInputChange}
-        error={exerciseData.weight <= 0}
-        helperText={exerciseData.weight <= 0 ? 'Weight must be at least 1' : ''}
-      ></TextField>
-      <TextField
-        label="Notes"
-        name="notes"
-        value={exerciseData.notes}
-        onChange={handleInputChange}
-      ></TextField>
-      <Button
-        startIcon={<DeleteIcon />}
-        onClick={() => deleteThisExercise(exercise.id)}
-      >
-        Delete
-      </Button>
-    </Stack>
+    <Grid container spacing={1}>
+      <Grid item>
+        <TextField
+          label="Name"
+          name="name"
+          value={exerciseData.name}
+          onChange={handleInputChange}
+          error={!exerciseData.name}
+          helperText={!exerciseData.name ? 'Name must be non-empty' : ''}
+        ></TextField>
+      </Grid>
+      <Grid item xs={3}>
+        <TextField
+          label="Sets"
+          name="sets"
+          value={exerciseData.sets}
+          type="number"
+          onChange={handleInputChange}
+          error={exerciseData.sets <= 0}
+          helperText={exerciseData.sets <= 0 ? 'Sets must be at least 1' : ''}
+        ></TextField>
+      </Grid>
+      <Grid item xs={3}>
+        <TextField
+          label="Reps"
+          name="reps"
+          value={exerciseData.reps}
+          type="number"
+          onChange={handleInputChange}
+          error={exerciseData.reps <= 0}
+          helperText={exerciseData.reps <= 0 ? 'Reps must be at least 1' : ''}
+        ></TextField>
+      </Grid>
+      <Grid item xs={3}>
+        <TextField
+          label="Weight"
+          name="weight"
+          value={exerciseData.weight}
+          type="number"
+          onChange={handleInputChange}
+          error={exerciseData.weight <= 0}
+          helperText={
+            exerciseData.weight <= 0 ? 'Weight must be at least 1' : ''
+          }
+        ></TextField>
+      </Grid>
+      <Grid item>
+        <TextField
+          label="Notes"
+          name="notes"
+          value={exerciseData.notes}
+          onChange={handleInputChange}
+        ></TextField>
+      </Grid>
+      <Grid item>
+        <Button
+          startIcon={<DeleteIcon />}
+          onClick={() => deleteThisExercise(exercise.id)}
+        >
+          Delete
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
