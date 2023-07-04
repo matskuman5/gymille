ae38fd1a6d105dae2c45f037fb42a75797006d52
Matias <selinmatias5@gmail.com>
2023-07-04 22:55:20 +0300

    uninstall jsonwebtoken

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -4,6 +4,4 @@ export const DATABASE_URL =
   process.env.DATABASE_URL ||
   'postgres://postgres:test@localhost:5432/postgres';
 
-export const JWT_SECRET = process.env.JWT_SECRET || 'test';
-
 export const REDIS_SECRET = process.env.REDIS_SECRET || 'test';
