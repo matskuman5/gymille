c2de57537bded13973565961ff9d48987e24d712
Matias <selinmatias5@gmail.com>
2023-07-04 22:58:38 +0300

    add redis and sleep command to script

--- backend/src/test/db-start.sh
+++ backend/src/test/db-start.sh
@@ -6,4 +6,6 @@ then
 else
     docker run -d --name testdb -e POSTGRES_PASSWORD=test -p 5432:5432 postgres
 fi
+docker run -d -p 6379:6379 -it redis/redis-stack-server:latest
+sleep 1s # wait for postgres to start up before connecting to it
 docker exec -it testdb psql -U postgres postgres
