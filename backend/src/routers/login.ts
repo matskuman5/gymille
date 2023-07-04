a477d23f0258d164e7e8491839c1dbc24e12769f
Matias <selinmatias5@gmail.com>
2023-07-04 23:09:56 +0300

    send status

--- backend/src/routers/login.ts
+++ backend/src/routers/login.ts
@@ -8,7 +8,7 @@ loginRouter.post('/', async (req, res) => {
   try {
     await login(req.body);
     req.session.username = req.body.username;
-    res.status(200);
+    res.status(200).send();
   } catch (error) {
     logger.error(error);
     res.status(400).json({ error });
