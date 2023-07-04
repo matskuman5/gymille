493d244feeb64c9cba06c06fb3d59ddfe56e0cf2
Matias <selinmatias5@gmail.com>
2023-07-04 21:16:38 +0300

    fix typo

--- backend/src/routers/login.ts
+++ backend/src/routers/login.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import logger from '../utils/logging';
-import { addUser } from '../services/users';
+import { login } from '../services/login';
 
 const loginRouter = Router();
 
 loginRouter.post('/', async (req, res) => {
   try {
-    const token = await addUser(req.body);
+    const token = await login(req.body);
     res.status(200).json(token);
   } catch (error) {
     logger.error(error);
