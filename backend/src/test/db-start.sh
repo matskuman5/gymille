bac3ee169012777aef1e89089e8b851383094925
Matias <selinmatias5@gmail.com>
2023-07-05 15:45:51 +0300

    start existing redis container if exists

--- backend/src/test/db-start.sh
+++ backend/src/test/db-start.sh
@@ -6,6 +6,11 @@ then
 else
     docker run -d --name testdb -e POSTGRES_PASSWORD=test -p 5432:5432 postgres
 fi
-docker run -d --name redistest -p 6379:6379 -it redis/redis-stack-server:latest
+if docker container ls -a --filter name=redistest --format '{{.Names}}' | grep -q '^redistest$';
+then
+    docker start redisdb
+else
+    docker run -d --name redistest -p 6379:6379 -it redis/redis-stack-server:latest
+fi
 sleep 1s # wait for postgres to start up before connecting to it
 docker exec -it testdb psql -U postgres postgres
