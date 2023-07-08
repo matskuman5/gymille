a0b29d42a6318de11bcf3a89e36611a647425071
Matias <selinmatias5@gmail.com>
2023-07-08 17:04:09 +0300

    add more info to ping endpoint

--- backend/src/routers/ping.ts
+++ backend/src/routers/ping.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import { NODE_ENV } from '../utils/config';
 
 const pingRouter = Router();
 
 pingRouter.get('/', (_req, res) => {
-  res.send('hi');
+  res.json({
+    message: 'OK',
+    nodeEnv: NODE_ENV,
+    nodeVersion: process.version,
+    uptimeSeconds: process.uptime(),
+    timestamp: new Date(),
+  });
 });
 
 export default pingRouter;
