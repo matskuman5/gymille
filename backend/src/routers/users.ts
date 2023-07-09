ca1a40e9d7e916caf2ad1d6b7f3400d73b558f34
Matias <selinmatias5@gmail.com>
2023-07-10 00:12:06 +0300

    remove deprecated error handling

--- backend/src/routers/users.ts
+++ backend/src/routers/users.ts
@@ -1,64 +1,39 @@
 import { Router } from 'express';
 import { addUser } from '../services/users';
-import { logger } from '../utils/logging';
 import { addSession, getUserSessions } from '../services/sessions';
 import { getUserSessionTemplates } from '../services/session-template';
 
 const userRouter = Router();
 
 userRouter.post('/', async (req, res) => {
-  try {
-    const response = await addUser(req.body);
-    res.status(201).json(response);
-  } catch (error) {
-    logger.error(error);
-    res.status(400).json({ error });
-  }
+  const response = await addUser(req.body);
+  res.status(201).json(response);
 });
 
 userRouter.get('/', async (req, res) => {
-  try {
-    console.log(req.session);
-    res.status(200).send({
-      username: req.session.username,
-      userId: req.session.userId,
-    });
-  } catch (error) {
-    logger.error(error);
-    res.status(400).json({ error });
-  }
+  console.log(req.session);
+  res.status(200).send({
+    username: req.session.username,
+    userId: req.session.userId,
+  });
 });
 
 userRouter.post('/:id/sessions', async (req, res) => {
-  try {
-    const response = await addSession({
-      ...req.body,
-      userId: req.params.id,
-    });
-    res.status(201).json(response);
-  } catch (error) {
-    logger.error(error);
-    res.status(400).json({ error });
-  }
+  const response = await addSession({
+    ...req.body,
+    userId: req.params.id,
+  });
+  res.status(201).json(response);
 });
 
 userRouter.get('/:id/sessions', async (req, res) => {
-  try {
-    const sessions = await getUserSessions(req.params.id);
-    res.json(sessions);
-  } catch (error) {
-    logger.error(error);
-    res.status(400).json({ error });
-  }
+  const sessions = await getUserSessions(req.params.id);
+  res.json(sessions);
 });
 
 userRouter.get('/:id/session-templates', async (req, res) => {
-  try {
-    const response = await getUserSessionTemplates(req.params.id);
-    res.json(response);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
+  const response = await getUserSessionTemplates(req.params.id);
+  res.json(response);
 });
 
 export default userRouter;
