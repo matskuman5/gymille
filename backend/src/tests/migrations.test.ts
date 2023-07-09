b2756b526a0a711be15df15d4aef98dc416135a1
Matias <selinmatias5@gmail.com>
2023-07-09 23:21:26 +0300

    add migration tests

---
+++ backend/src/tests/migrations.test.ts
@@ -0,0 +1,27 @@
+import { migrator, resetMigrations, seedData } from '../utils/umzug';
+import { connectToPostgres, sequelize } from '../utils/db';
+
+describe('migration', () => {
+  beforeAll(async () => {
+    await connectToPostgres();
+  });
+
+  test('correct number of executed and pending migrations', async () => {
+    const executedAmount = (await migrator.executed()).length;
+    const pendingAmount = (await migrator.pending()).length;
+    await expect(executedAmount).toBe(0);
+    await expect(pendingAmount).toBeGreaterThan(0);
+  });
+
+  test('runs migrations successfully', async () => {
+    await expect(resetMigrations()).resolves.not.toThrow();
+  });
+
+  test('adds seed data successfully', async () => {
+    await expect(seedData()).resolves.not.toThrow();
+  });
+
+  afterAll(() => {
+    sequelize.close();
+  });
+});
