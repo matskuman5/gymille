73353212e8298d6aade4f103d6ee4d73172563ad
Matias <selinmatias5@gmail.com>
2023-07-09 18:02:15 +0300

    remove deprecated sync code

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,35 +1,8 @@
 import { SessionModel } from './session';
 import { ExerciseModel } from './exercise';
-import { logger } from '../utils/logging';
 import { SessionTemplateModel } from './session-template';
 import { ExerciseTemplateModel } from './exercise-template';
 import { UserModel } from './user';
-
-const setupModels = async () => {
-  try {
-    SessionModel.hasMany(ExerciseModel);
-    ExerciseModel.belongsTo(SessionModel);
-
-    UserModel.hasMany(SessionModel);
-    SessionModel.belongsTo(UserModel);
-
-    SessionTemplateModel.hasMany(ExerciseTemplateModel);
-    ExerciseTemplateModel.belongsTo(SessionTemplateModel);
-
-    UserModel.hasMany(SessionTemplateModel);
-    SessionTemplateModel.belongsTo(UserModel);
-
-    await SessionModel.sync({ force: true });
-    await ExerciseModel.sync({ force: true });
-    await SessionTemplateModel.sync({ force: true });
-    await ExerciseTemplateModel.sync({ force: true });
-    await UserModel.sync({ force: true });
-
-    logger.info('Models synced with database successfully');
-  } catch (error) {
-    logger.error(error);
-  }
-};
 
 export default {
   SessionModel,
@@ -37,5 +10,4 @@ export default {
   SessionTemplateModel,
   ExerciseTemplateModel,
   UserModel,
-  setupModels,
 };
