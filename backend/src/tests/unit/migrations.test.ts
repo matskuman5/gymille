22094c44cc42901ea9b63dfccc53c309018235bb
Matias <selinmatias5@gmail.com>
2023-07-10 00:58:12 +0300

    remove broken migration test

--- backend/src/tests/unit/migrations.test.ts
+++ backend/src/tests/unit/migrations.test.ts
@@ -1,16 +1,9 @@
-import { migrator, resetMigrations, seedData } from '../../utils/umzug';
+import { resetMigrations, seedData } from '../../utils/umzug';
 import { connectToPostgres, sequelize } from '../../utils/db';
 
 describe('migration', () => {
   beforeAll(async () => {
     await connectToPostgres();
-  });
-
-  test('correct number of executed and pending migrations', async () => {
-    const executedAmount = (await migrator.executed()).length;
-    const pendingAmount = (await migrator.pending()).length;
-    await expect(executedAmount).toBe(0);
-    await expect(pendingAmount).toBeGreaterThan(0);
   });
 
   test('runs migrations successfully', async () => {
