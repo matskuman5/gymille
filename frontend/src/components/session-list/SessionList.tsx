72d0e93bd06df07ca45f54668fd0a175adfe50f3
Matias <selinmatias5@gmail.com>
2023-07-10 23:57:28 +0300

    use queries in session list

--- frontend/src/components/session-list/SessionList.tsx
+++ frontend/src/components/session-list/SessionList.tsx
@@ -1,58 +1,57 @@
-import { useContext, useEffect, useState } from 'react';
-import { Session } from '../../types';
 import SessionItem from './SessionItem';
 import { getUserSessions } from '../../services/sessions';
 import { Stack, Typography } from '@mui/material';
 import { deleteSession as deleteSessionAPI } from '../../services/sessions';
 import Loading from '../Loading';
-import { UserContext } from '../user-authentication/UserContext';
+import { useQuery } from '@tanstack/react-query';
+import { getUserData } from '../../services/user';
 
 const SessionList = () => {
-  const [sessions, setSessions] = useState<Session[]>([]);
-  const [loading, setLoading] = useState(true);
+  const { data: userData } = useQuery({
+    queryKey: ['userData'],
+    queryFn: getUserData,
+  });
 
-  const { userId } = useContext(UserContext);
-
-  const fetchSessions = async () => {
-    setLoading(true);
-    const sessionsData = await getUserSessions(userId);
-    setLoading(false);
-    if (sessionsData !== undefined) {
-      setSessions(sessionsData);
-    }
-  };
-
-  useEffect(() => {
-    if (userId) {
-      fetchSessions();
-    }
-  }, []);
+  const {
+    data: sessions,
+    isLoading,
+    isSuccess,
+  } = useQuery({
+    queryKey: ['sessions'],
+    queryFn: () => getUserSessions(userData!.userId),
+    enabled: !!userData?.userId,
+  });
 
   const deleteSession = async (id: string) => {
     await deleteSessionAPI(id);
-    await fetchSessions();
   };
 
   return (
     <>
-      {userId ? (
+      {userData ? (
         <>
-          {loading ? (
+          {isLoading ? (
             <Loading text={'Fetching sessions...'} />
           ) : (
             <>
-              {sessions.length === 0 ? (
-                <Typography variant="h5">No sessions created yet!</Typography>
-              ) : (
-                <Stack spacing={2}>
-                  {sessions.map((session) => (
-                    <SessionItem
-                      key={session.id}
-                      session={session}
-                      deleteSession={deleteSession}
-                    ></SessionItem>
-                  ))}
-                </Stack>
+              {isSuccess && (
+                <>
+                  {sessions.length === 0 ? (
+                    <Typography variant="h5">
+                      No sessions created yet!
+                    </Typography>
+                  ) : (
+                    <Stack spacing={2}>
+                      {sessions.map((session) => (
+                        <SessionItem
+                          key={session.id}
+                          session={session}
+                          deleteSession={deleteSession}
+                        ></SessionItem>
+                      ))}
+                    </Stack>
+                  )}
+                </>
               )}
             </>
           )}
