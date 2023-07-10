110a1704b03bab85848097e9f190da525c00d28f
Matias <selinmatias5@gmail.com>
2023-07-10 18:47:27 +0300

    centralize axios error handling

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,24 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { showError } from './notifications';
 
 const instance = axios.create({
   withCredentials: true,
 });
+
+interface ErrorResponse {
+  error?: string;
+}
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError<ErrorResponse>) => {
+    if (error.response?.data.error) {
+      showError(`Error: ${error.response.data.error}`);
+    } else {
+      showError(`Error: ${error}`);
+    }
+    throw error;
+  }
+);
 
 export default instance;
