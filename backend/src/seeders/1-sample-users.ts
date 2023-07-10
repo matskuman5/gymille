88309ec37001967ccbefcd68f166b305048263c0
Matias <selinmatias5@gmail.com>
2023-07-10 18:56:28 +0300

    add actual sample user passwords

--- backend/src/seeders/1-sample-users.ts
+++ backend/src/seeders/1-sample-users.ts
@@ -4,12 +4,16 @@ const seedUsers = [
   {
     id: 'ff3e694e-02fd-413b-a38f-b826cc2dc08b',
     username: 'GymBro1',
-    passwordHash: 'jfiejifjaj',
+    // password plaintext: password123
+    passwordHash:
+      '$2a$10$6/ynFnmxxPwFWQ6s.iHYves/YTWU8SWjrgBrO8we4FwRzV2Q5BTHG',
   },
   {
     id: '0293f428-3739-4d85-b7c3-9bbbc8973fed',
     username: 'Grizzly69',
-    passwordHash: 'fjawifjaoifj',
+    // password plaintext: password456
+    passwordHash:
+      '$2a$10$/6Pesn73kPgm4lOJS6sMaOXUpAXsWWtiEnO3BWzrlwG7Pnhtlj7PK',
   },
 ];
 
