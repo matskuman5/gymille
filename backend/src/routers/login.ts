e19cc755fc29d49bbd3363bbffb8cc537c62edad
Matias <selinmatias5@gmail.com>
2023-07-10 19:02:18 +0300

    remove try catch blocks from login/logout routers

--- backend/src/routers/login.ts
+++ backend/src/routers/login.ts
@@ -1,22 +1,16 @@
 import { Router } from 'express';
-import { logger } from '../utils/logging';
 import { validateLoginAndReturnId } from '../services/login';
 
 const loginRouter = Router();
 
 loginRouter.post('/', async (req, res) => {
-  try {
-    const userId = await validateLoginAndReturnId(req.body);
-    req.session.userId = userId;
-    req.session.username = req.body.username;
-    res.status(200).send({
-      username: req.session.username,
-      userId: req.session.userId,
-    });
-  } catch (error) {
-    logger.error(error);
-    res.status(400).json({ error });
-  }
+  const userId = await validateLoginAndReturnId(req.body);
+  req.session.userId = userId;
+  req.session.username = req.body.username;
+  res.status(200).send({
+    username: req.session.username,
+    userId: req.session.userId,
+  });
 });
 
 export default loginRouter;
--- backend/src/routers/logout.ts
+++ backend/src/routers/logout.ts
@@ -1,17 +1,11 @@
 import { Router } from 'express';
-import { logger } from '../utils/logging';
 
 const logoutRouter = Router();
 
 logoutRouter.post('/', async (req, res) => {
-  try {
-    req.session.destroy(() => {
-      res.status(200).send();
-    });
-  } catch (error) {
-    logger.error(error);
-    res.status(400).json({ error });
-  }
+  req.session.destroy(() => {
+    res.status(200).send();
+  });
 });
 
 export default logoutRouter;
