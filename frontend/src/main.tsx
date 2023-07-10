34cb7a84942e3f8fb4c56fc6d40a56a3de86dff2
Matias <selinmatias5@gmail.com>
2023-07-10 22:05:38 +0300

    wrap app in queryclientprovider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,16 +5,21 @@ import { BrowserRouter } from 'react-router-dom';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { UserProvider } from './components/user-authentication/UserContext.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <UserProvider>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <BrowserRouter basename="/front">
-          <App />
-        </BrowserRouter>
-      </LocalizationProvider>
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <BrowserRouter basename="/front">
+            <App />
+          </BrowserRouter>
+        </LocalizationProvider>
+      </UserProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
