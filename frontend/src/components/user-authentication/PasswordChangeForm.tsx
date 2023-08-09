4574fd2370ed81a8314a29289c3c3c2b27b41589
Matias <selinmatias5@gmail.com>
2023-08-09 16:28:54 +0300

    redirect user to login page if not logged in

--- frontend/src/components/user-authentication/PasswordChangeForm.tsx
+++ frontend/src/components/user-authentication/PasswordChangeForm.tsx
@@ -2,6 +2,7 @@ import { TextField, Button, Stack, Typography } from '@mui/material';
 import { useState } from 'react';
 import { getUserData, updatePassword } from '../../services/user';
 import { useQuery } from '@tanstack/react-query';
+import { Navigate } from 'react-router-dom';
 
 const PasswordChangeForm = () => {
   const [newPassword, setNewPassword] = useState('');
@@ -12,27 +13,33 @@ const PasswordChangeForm = () => {
   });
 
   return (
-    <Stack spacing={2}>
-      <Typography variant="h5">Change password</Typography>
-      <TextField
-        label="New Password"
-        type="password"
-        value={newPassword}
-        onChange={(event) => setNewPassword(event.target.value)}
-        error={newPassword.length < 8}
-        helperText={
-          newPassword.length < 8
-            ? 'Password must be at least 8 characters long'
-            : ''
-        }
-      />
-      <Button
-        variant="contained"
-        onClick={() => updatePassword(userData!.userId, newPassword)}
-      >
-        Change Password
-      </Button>
-    </Stack>
+    <>
+      {userData ? (
+        <Stack spacing={2}>
+          <Typography variant="h5">Change password</Typography>
+          <TextField
+            label="New Password"
+            type="password"
+            value={newPassword}
+            onChange={(event) => setNewPassword(event.target.value)}
+            error={newPassword.length < 8}
+            helperText={
+              newPassword.length < 8
+                ? 'Password must be at least 8 characters long'
+                : ''
+            }
+          />
+          <Button
+            variant="contained"
+            onClick={() => updatePassword(userData.userId, newPassword)}
+          >
+            Change Password
+          </Button>
+        </Stack>
+      ) : (
+        <Navigate to="/user" />
+      )}
+    </>
   );
 };
 
