a78a86e6461441a9ea3edac5c601fccd2e44787d
Matias <selinmatias5@gmail.com>
2023-08-09 14:54:14 +0300

    remove deprecated try catch blocks

--- backend/src/routers/session-templates.ts
+++ backend/src/routers/session-templates.ts
@@ -9,39 +9,23 @@ import {
 const sessionTemplateRouter = Router();
 
 sessionTemplateRouter.get('/', async (_req, res) => {
-  try {
-    const response = await getAllSessionTemplates();
-    res.json(response);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
+  const response = await getAllSessionTemplates();
+  res.json(response);
 });
 
 sessionTemplateRouter.post('/', async (req, res) => {
-  try {
-    const response = await addSessionTemplate(req.body);
-    res.status(201).json(response);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
+  const response = await addSessionTemplate(req.body);
+  res.status(201).json(response);
 });
 
 sessionTemplateRouter.put('/:id', async (req, res) => {
-  try {
-    const response = await updateSessionTemplate(req.params.id, req.body);
-    res.json(response);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
+  const response = await updateSessionTemplate(req.params.id, req.body);
+  res.json(response);
 });
 
 sessionTemplateRouter.delete('/:id', async (req, res) => {
-  try {
-    const response = await deleteSessionTemplate(req.params.id);
-    res.json(response);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
+  const response = await deleteSessionTemplate(req.params.id);
+  res.json(response);
 });
 
 export default sessionTemplateRouter;
