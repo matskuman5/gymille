12e5a434a255af317f6955760b637003ec65e5c6
Matias <selinmatias5@gmail.com>
2023-08-09 21:56:22 +0300

    remove unused endpoint

--- backend/src/routers/user-session-templates.ts
+++ backend/src/routers/user-session-templates.ts
@@ -1,18 +1,7 @@
 import { Router } from 'express';
-import {
-  addSessionTemplate,
-  getUserSessionTemplates,
-} from '../services/session-template';
+import { getUserSessionTemplates } from '../services/session-template';
 
 const userSessionTemplatesRouter = Router();
-
-userSessionTemplatesRouter.post('/:id/session-templates', async (req, res) => {
-  const response = await addSessionTemplate({
-    ...req.body,
-    userId: req.params.id,
-  });
-  res.status(201).json(response);
-});
 
 userSessionTemplatesRouter.get('/:id/session-templates', async (req, res) => {
   const response = await getUserSessionTemplates(req.params.id);
