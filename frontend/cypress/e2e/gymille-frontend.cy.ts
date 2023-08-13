474a509425bd2f3e442801aa8f1bf303e0c0cef9
Matias <selinmatias5@gmail.com>
2023-08-13 15:49:40 +0300

    add login warning test

--- frontend/cypress/e2e/gymille-frontend.cy.ts
+++ frontend/cypress/e2e/gymille-frontend.cy.ts
@@ -3,4 +3,12 @@ describe('Basic ', function () {
     cy.visit('http://localhost:5173/front');
     cy.contains('Gymille!');
   });
+
+  it('login warning functions properly', function () {
+    cy.visit('http://localhost:5173/front');
+    cy.contains('Dismiss').click();
+    cy.contains('Dismiss').should('not.exist');
+    cy.reload();
+    cy.contains('Dismiss').should('not.exist');
+  });
 });
