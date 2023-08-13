1e5c8990be6eb94d161648d064e4a39ee8b3a6d4
Matias <selinmatias5@gmail.com>
2023-08-13 15:38:11 +0300

    remove typo

--- frontend/cypress/e2e/gymille-frontend.cy.ts
+++ frontend/cypress/e2e/gymille-frontend.cy.ts
@@ -2,8 +2,5 @@ describe('Basic ', function () {
   it('front page can be opened', function () {
     cy.visit('http://localhost:5173/front');
     cy.contains('Gymille!');
-    cy.contains(
-      'Note app, Department of Computer Science, University of Helsinki 2023'
-    );
   });
 });
