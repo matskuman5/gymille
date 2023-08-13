41e0acdffdb72c0bb60f60a67efb4edac4cb2253
Matias <selinmatias5@gmail.com>
2023-08-13 15:36:04 +0300

    install cypress eslint plugin

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, 'cypress/globals': true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -8,7 +8,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh', '@tanstack/query'],
+  plugins: ['react-refresh', '@tanstack/query', 'cypress'],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
